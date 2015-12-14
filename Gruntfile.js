1188d5ad1e87714690fc0f1954bd97fe68716b21
Jeppe Bårris <jeppe@redpitt.com>
2015-12-14 01:00:56 +0100

    Disable imagemin + svgmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -431,8 +431,8 @@ module.exports = function (grunt) {
     'cssmin',
     'autoprefixer:dist',
     'uglify',
-    'imagemin',
-    'svgmin',
+    // 'imagemin',
+    // 'svgmin',
     'filerev',
     'usemin',
     'htmlmin'
